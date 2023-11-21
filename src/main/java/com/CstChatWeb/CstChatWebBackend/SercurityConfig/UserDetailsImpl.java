f3b5c124c3f144280f7c95fae8ba7eb06697abe4
Cao Bá Hướng <122510132+anticoder07@users.noreply.github.com>
2023-11-21 17:27:18 +0700

    update sercurity v1.0

---
+++ src/main/java/com/CstChatWeb/CstChatWebBackend/SercurityConfig/UserDetailsImpl.java
@@ -0,0 +1,51 @@
+package com.CstChatWeb.CstChatWebBackend.SercurityConfig;
+
+import com.CstChatWeb.CstChatWebBackend.entity.profiles.User;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+
+@AllArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+	@Getter
+	@Setter
+	private User user;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		return user.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		return null;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return false;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return false;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return false;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return false;
+	}
+}
